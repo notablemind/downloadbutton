15bff604db5ffc624e65e64c8d1d5db018820e9c
Chris Hiester <Christopher_Heister@comcast.com>
2016-09-07 10:33:35 -0400

    fixed deprecated addons require

--- index.js
+++ index.js
@@ -1,89 +1,78 @@
-
-var React = require('react/addons')
-  , cx = React.addons.classSet
-  , PT = React.PropTypes
-
-  , saveAs = require('./save-as')
+var React = require('react');
+var addons = require('react-addons');
+var cx = React.addons.classSet,
+    PT = React.PropTypes,
+    saveAs = require('./save-as')
 
 var DownloadButton = React.createClass({
-  propTypes: {
-    fileData: PT.object,
-    genFile: PT.func,
-    async: PT.bool,
-    generateTitle: PT.string,
-    downloadTitle: PT.oneOfType([PT.string, PT.func]),
-    loadingTitle: PT.string,
-    onDownloaded: PT.func,
-  },
+    propTypes: {
+        fileData: PT.object,
+        genFile: PT.func,
+        async: PT.bool,
+        generateTitle: PT.string,
+        downloadTitle: PT.oneOfType([PT.string, PT.func]),
+        loadingTitle: PT.string,
+        onDownloaded: PT.func
+    },
 
-  getDefaultProps: function () {
-    return {
-      async: false,
-      downloadTitle: 'Download',
-      generateTitle: 'Generate file',
-      loadingTitle: 'Loading...',
-    }
-  },
+    getDefaultProps: function() {
+        return {async: false, downloadTitle: 'Download', generateTitle: 'Generate file', loadingTitle: 'Loading...'}
+    },
 
-  getInitialState: function () {
-    return {
-      loading: false,
-      fileData: null,
-    }
-  },
+    getInitialState: function() {
+        return {loading: false, fileData: null}
+    },
 
-  _onGenerate: function () {
-    this.setState({loading: true, fileData: null})
-    this.props.genFile(this._donePreparing)
-  },
+    _onGenerate: function() {
+        this.setState({loading: true, fileData: null})
+        this.props.genFile(this._donePreparing)
+    },
 
-  _donePreparing: function (fileData) {
-    this.setState({
-      loading: false,
-      fileData: fileData,
+    _donePreparing: function(fileData) {
+        this.setState({loading: false, fileData: fileData})
+    },
+
+    _onDownload: function() {
+        var fileData = this.props.fileData || (this.props.async
+                ? this.state.fileData
+                : this.props.genFile())
+            if (!fileData) {
+                return false
+            }
+            var blob = new Blob([fileData.contents], {type: fileData.mime}),
+                url = URL.createObjectURL(blob)
+            saveAs(url, fileData.filename)
+            this.props.onDownloaded && this.props.onDownloaded()
+        },
+
+        render: function() {
+            // need one or the other
+            if (!this.props.genFile && !this.props.fileData) {
+                return <em>Invalid configuration for download button</em>
+            }
+            var style = this.props.style,
+                cls = 'DownloadButton ' + (this.props.className || '')
+
+            if (this.props.fileData || !this.props.async || this.state.fileData) {
+                var title = this.props.downloadTitle
+                if ('function' === typeof title) {
+                    title = title(this.props.fileData || this.state.fileData)
+                }
+                return <button style={style} onClick={this._onDownload} className={cls}>
+                    {title}
+                </button>
+            }
+
+            if (this.state.loading) {
+                return <button style={style} className={cls + ' DownloadButton-loading'}>
+                    {this.props.loadingTitle}
+                </button>
+            }
+
+            return <button style={style} onClick={this._onGenerate} className={cls + ' DownloadButton-generate'}>
+                {this.props.generateTitle}
+            </button>
+        }
     })
-  },
 
-  _onDownload: function () {
-    var fileData = this.props.fileData || (this.props.async ? this.state.fileData : this.props.genFile())
-    if (!fileData) {
-      return false
-    }
-    var blob = new Blob([fileData.contents], {type: fileData.mime})
-      , url = URL.createObjectURL(blob)
-    saveAs(url, fileData.filename)
-    this.props.onDownloaded && this.props.onDownloaded()
-  },
-
-  render: function () {
-    // need one or the other
-    if (!this.props.genFile && !this.props.fileData) {
-      return <em>Invalid configuration for download button</em>
-    }
-    var style = this.props.style
-      , cls = 'DownloadButton ' + (this.props.className || '')
-
-    if (this.props.fileData || !this.props.async || this.state.fileData) {
-      var title = this.props.downloadTitle
-      if ('function' === typeof title) {
-        title = title(this.props.fileData || this.state.fileData)
-      }
-      return <button style={style} onClick={this._onDownload} className={cls}>
-        {title}
-      </button>
-    }
-
-    if (this.state.loading) {
-      return <button style={style} className={cls + ' DownloadButton-loading'}>
-        {this.props.loadingTitle}
-      </button>
-    }
-
-    return <button style={style} onClick={this._onGenerate} className={cls + ' DownloadButton-generate'}>
-      {this.props.generateTitle}
-    </button>
-  }
-})
-
-module.exports = DownloadButton
-
+    module.exports = DownloadButton
